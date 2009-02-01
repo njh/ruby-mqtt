33d24707cf9fefbb4cea9aa99944fc61849dcb2d
Nicholas J Humfrey <njh@aelius.com>
2009-02-01 02:02:38 +0000

    More specifications for the Client class. Only code without specifications is inside Thread.new block.

--- spec/mqtt_client_spec.rb
+++ spec/mqtt_client_spec.rb
@@ -8,23 +8,67 @@ describe MQTT::Client do
   before(:each) do
     @client = MQTT::Client.new
     @socket = StringIO.new
-    @client.instance_variable_set(:@socket, @socket)
-    @client.instance_variable_set(:@read_thread, stub_everything('Read Thread'))
   end
   
   describe "when calling the 'connect' method" do
-  
+    before(:each) do
+      TCPSocket.stubs(:new).returns(@socket)
+      Thread.stubs(:new)
+    end
+    
+    it "should create a TCP Socket if not connected" do
+      TCPSocket.expects(:new).once.returns(@socket)
+      @client.connect('myclient')
+    end
+    
+    it "should not create a new TCP Socket if connected" do
+      @client.stubs(:connected?).returns(true)
+      TCPSocket.expects(:new).never
+      @client.connect('myclient')
+    end
+    
+    it "should start the reader thread if not connected" do
+      Thread.expects(:new).once
+      @client.connect('myclient')
+    end
+    
+    it "should write a valid CONNECT packet to the socket if not connected" do
+      @client.connect('myclient')
+      @socket.string.should == "\020\026\x00\x06MQIsdp\x03\x00\x00\x0a\x00\x08myclient"
+    end
+    
+    it "should disconnect after connecting, if a block is given" do
+      @client.expects(:disconnect).once
+      @client.connect('myclient') { nil }
+    end
+    
+    it "should not disconnect after connecting, if no block is given" do
+      @client.expects(:disconnect).never
+      @client.connect('myclient')
+    end
+    
   end
   
   describe "when calling the 'disconnect' method" do
-    it "should not do anything if the socket is already disconnected"
+    before(:each) do
+      @client.instance_variable_set(:@socket, @socket)
+      @client.instance_variable_set(:@read_thread, stub_everything('Read Thread'))
+    end
   
-    it "should write a valid DISCONNECT packet to the socket if the send_msg=true" do
+    it "should not do anything if the socket is already disconnected" do
+      @client.stubs(:connected?).returns(false)
+      @client.disconnect(true)
+      @socket.string.should == ""
+    end
+  
+    it "should write a valid DISCONNECT packet to the socket if connected and the send_msg=true an" do
+      @client.stubs(:connected?).returns(true)
       @client.disconnect(true)
       @socket.string.should == "\xE0\x00"
     end
     
     it "should not write anything to the socket if the send_msg=false" do
+      @client.stubs(:connected?).returns(true)
       @client.disconnect(false)
       @socket.string.should be_empty
     end
@@ -36,6 +80,10 @@ describe MQTT::Client do
   end
   
   describe "when calling the 'ping' method" do
+    before(:each) do
+      @client.instance_variable_set(:@socket, @socket)
+    end
+
     it "should write a valid PINGREQ packet to the socket" do
       @client.ping
       @socket.string.should == "\xC0\x00"
@@ -49,6 +97,10 @@ describe MQTT::Client do
   end
   
   describe "when calling the 'publish' method" do
+    before(:each) do
+      @client.instance_variable_set(:@socket, @socket)
+    end
+
     it "should write a valid PUBLISH packet to the socket without the detain flag" do
       @client.publish('topic','payload', false, 0)
       @socket.string.should == "\x30\x0e\x00\x05topicpayload"
@@ -71,28 +123,36 @@ describe MQTT::Client do
   end
 
   describe "when calling the 'subscribe' method" do
-    it "should write a valid SUBSCRIBE packet with QoS 0 to the socket when given a single topic String" do
+    before(:each) do
+      @client.instance_variable_set(:@socket, @socket)
+    end
+
+    it "should write a valid SUBSCRIBE packet with QoS 0 to the socket if given a single topic String" do
       @client.subscribe('a/b')
       @socket.string.should == "\x82\x08\x00\x01\x00\x03a/b\x00"
     end
 
-    it "should write a valid SUBSCRIBE packet with QoS 0 to the socket when given a two topic Strings in an Array" do
+    it "should write a valid SUBSCRIBE packet with QoS 0 to the socket if given a two topic Strings in an Array" do
       @client.subscribe('a/b','c/d')
       @socket.string.should == "\x82\x0e\x00\x01\x00\x03a/b\x00\x00\x03c/d\x00"
     end
 
-    it "should write a valid SUBSCRIBE packet to the socket when given a two topic Strings with QoS in an Array" do
+    it "should write a valid SUBSCRIBE packet to the socket if given a two topic Strings with QoS in an Array" do
       @client.subscribe(['a/b',0],['c/d',1])
       @socket.string.should == "\x82\x0e\x00\x01\x00\x03a/b\x00\x00\x03c/d\x01"
     end
 
-    it "should write a valid SUBSCRIBE packet to the socket when given a two topic Strings with QoS in a Hash" do
+    it "should write a valid SUBSCRIBE packet to the socket if given a two topic Strings with QoS in a Hash" do
       @client.subscribe('a/b' => 0,'c/d' => 1)
       @socket.string.should == "\x82\x0e\x00\x01\x00\x03a/b\x00\x00\x03c/d\x01"
     end
   end
 
   describe "when calling the 'get' method" do
+    before(:each) do
+      @client.instance_variable_set(:@socket, @socket)
+    end
+
     def inject_packet(topic, payload, opts={})
       opts[:type] = :publish
       packet = MQTT::Packet.new(opts)
@@ -118,16 +178,62 @@ describe MQTT::Client do
   end
 
   describe "when calling the 'unsubscribe' method" do
-    it "should write a valid UNSUBSCRIBE packet to the socket when given a single topic String" do
+    before(:each) do
+      @client.instance_variable_set(:@socket, @socket)
+    end
+
+    it "should write a valid UNSUBSCRIBE packet to the socket if given a single topic String" do
       @client.unsubscribe('a/b')
       @socket.string.should == "\xa2\x05\x00\x03a/b"
     end
     
-    it "should write a valid UNSUBSCRIBE packet to the socket when given a two topic Strings" do
+    it "should write a valid UNSUBSCRIBE packet to the socket if given a two topic Strings" do
       @client.unsubscribe('a/b','c/d')
       @socket.string.should == "\xa2\x0a\x00\x03a/b\x00\x03c/d"
     end
   end
   
+  describe "when calling the 'receive_packet' method" do
+    before(:each) do
+      @client.instance_variable_set(:@socket, @socket)
+      IO.stubs(:select).returns([[@socket], [], []])
+      @read_queue = @client.instance_variable_get(:@read_queue)
+      @parent_thread = Thread.current[:parent] = stub_everything('Parent Thread')
+    end
+
+    it "should put PUBLISH messages on to the read queue" do
+      @socket.write("\x30\x0e\x00\x05topicpayload")
+      @socket.rewind
+      @client.send(:receive_packet)
+      @read_queue.size.should == 1
+    end
+
+    it "should not put other messages on to the read queue" do
+      @socket.write("\x20\x02\x00\x00")
+      @socket.rewind
+      @client.send(:receive_packet)
+      @read_queue.size.should == 0
+    end
+    
+    it "should send a ping packet if one is due" do
+      IO.expects(:select).returns(nil)
+      @client.instance_variable_set(:@last_pingreq, Time.at(0))
+      @client.expects(:ping).once
+      @client.send(:receive_packet)
+    end
+
+    it "should close the socket if there is an exception" do
+      @socket.expects(:close).once
+      MQTT::Packet.stubs(:read).raises(MQTT::Exception)
+      @client.send(:receive_packet)
+    end
+
+    it "should pass exceptions up to parent thread" do
+      @parent_thread.expects(:raise).once
+      MQTT::Packet.stubs(:read).raises(MQTT::Exception)
+      @client.send(:receive_packet)
+    end
+    
+  end
   
 end
