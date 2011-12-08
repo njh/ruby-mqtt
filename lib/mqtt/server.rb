b2b1b843c0071f44b7e44ae25ea6e3ef44b4fc9c
Nicholas J Humfrey <njh@aelius.com>
2011-12-08 10:05:49 +0000

    Implemented command line options parsing for rmqtt server.

--- lib/mqtt/server.rb
+++ lib/mqtt/server.rb
@@ -1,16 +1,51 @@
 require 'logger'
+require 'optparse'
 
 class MQTT::Server
   attr_accessor :address
   attr_accessor :port
   attr_accessor :logger
 
-  def initialize(argv)
-    # FIXME: do options parsing here
+  def initialize(args=[])
+    # Set defaults
     self.address = "0.0.0.0"
     self.port = MQTT::DEFAULT_PORT
     self.logger = Logger.new(STDOUT)
     self.logger.level = Logger::INFO
+    parse(args) unless args.empty?
+  end
+
+  def parse(args)
+    OptionParser.new("", 24, '  ') do |opts|
+      opts.banner = "Usage: #{File.basename $0} [options]"
+
+      opts.separator ""
+      opts.separator "Options:"
+
+      opts.on("-D", "--debug", "turn on debug logging") do
+        self.logger.level = Logger::DEBUG
+      end
+
+      opts.on("-a", "--address [HOST]", "bind to HOST address (default: #{address})") do |address|
+        self.address = address
+      end
+
+      opts.on("-p", "--port [PORT]", "port number to run on (default: #{port})") do |port|
+        self.port = port
+      end
+
+      opts.on_tail("-h", "--help", "show this message") do
+        puts opts
+        exit
+      end
+
+      opts.on_tail("--version", "show version") do
+        puts MQTT::VERSION
+        exit
+      end
+
+      opts.parse!(args)
+    end
   end
 
   def run
