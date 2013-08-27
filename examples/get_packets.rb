8649f031faf56570a9f538e8be65063b56264f8f
Nicholas J Humfrey <njh@aelius.com>
2013-08-27 21:26:02 +0100

    Created an example of using get_packet (for #10)

---
+++ examples/get_packets.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+#
+# Connect to a MQTT broker, subscribe to all topics
+# and then loop, displaying the packets received.
+#
+
+$:.unshift File.dirname(__FILE__)+'/../lib'
+
+require 'rubygems'
+require 'mqtt'
+
+MQTT::Client.connect('test.mosquitto.org') do |client|
+  # If you pass a block to the get_packet method, then it will loop
+  client.get_packet('#') do |packet|
+    p packet
+  end
+end
