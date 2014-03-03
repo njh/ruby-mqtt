719bb861f18901b8f6b96e8032e01c52cc9bc77c
Nicholas Humfrey <njh@aelius.com>
2014-03-03 17:33:13 +0000

    Made the ruby authentication example less specific to 2lemetry (fixes #29)

--- examples/authentication.rb
+++ examples/authentication.rb
@@ -6,28 +6,26 @@ $:.unshift File.dirname(__FILE__)+'/../lib'
 
 require 'rubygems'
 require 'mqtt'
-require 'md5'
 
-# The q.m2m.io server uses md5 hashed versions of passwords
-def md5(s)
-	Digest::MD5.hexdigest(s)
-end
 
-MQTT::Client.connect(:remote_host => 'q.m2m.io', :username => 'tim+rubytest@2lemetry.com', :password => md5('P@55w0rd')) do |client|
+MQTT::Client.connect(
+  :remote_host => 'test.mosquitto.org',
+  :username => 'test',
+  :password => 'password'
+) do |client|
 	puts 'connected'
 
 	# We have to do this in a separate thread or process (or a different computer)
 	Thread.new do
 		20.times do # We could do it forever, but 20 times is good enough
 			sleep(0.5)	# slow it down because computers are too fast
-			client.publish('2be57b94bddfae1341d89852fdd6f15b/test/ruby', "The time is now #{Time.now}")
+			client.publish('test/ruby/authentication', "The time is now #{Time.now}")
 		end
 	end
 
 	# when a block is passed to #get, it loops infinitely so this has to be the last line of our program
-	client.get("2be57b94bddfae1341d89852fdd6f15b/test/#") do |topic, msg|
+	client.get("test/ruby/#") do |topic, msg|
 		puts "Got message '#{msg}' on topic '#{topic}'"
 	end
 
 end
-
