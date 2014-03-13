37199efa50646f576f8407a92b5d7420641ca0eb
gabriel-rea-tierconnect-com <gabriel.rea@tierconnect.com>
2014-03-12 23:20:05 -0400

    Adding rake file

--- Rakefile
+++ Rakefile
@@ -16,6 +16,18 @@ namespace :spec do
   end
 end
 
+desc "Run the specs."
+RSpec::Core::RakeTask.new(:test_311,:host,:port) do |t,args|
+  usage_msg = "Usage: rake test_311[MQTT_BROKER_ADDRESS,MQTT_PORT]
+  i.e. $ rake test_311[192.168.0.3,1883]"
+  raise usage_msg if args[:host].nil?
+
+  p args
+  ENV['MQTT_HOST'] = args[:host]
+  ENV['MQTT_PORT'] = args[:port] || '1883'
+  t.pattern = "spec/tc/test_subscribe.rb"
+end
+
 namespace :doc do
   YARD::Rake::YardocTask.new
 
--- spec/tc/test_subscribe.rb
+++ spec/tc/test_subscribe.rb
@@ -4,32 +4,37 @@ require 'bundler'
 Bundler.require(:default, :development)
 require 'mqtt'
 
-# HOST = 'localhost'
-HOST = '10.100.0.139'
+# MQTT_HOST = 'localhost'
+MQTT_HOST = ENV['MQTT_HOST'] || 'localhost'
+MQTT_PORT = ENV['MQTT_PORT'] || 1883
 
 def create_standard_client()
-  client = MQTT::Client.connect({remote_host: HOST,remote_port: 1883, :reconnect => false, :v311 => true, :clean_session => true,:keep_alive=>2})
+  client = MQTT::Client.connect({remote_host: MQTT_HOST,:remote_port=>MQTT_PORT, :reconnect => false, :v311 => true, :clean_session => true,:keep_alive=>2})
   return client
 end
 
 def create_unclean_client()
-  client = MQTT::Client.connect({remote_host: HOST,remote_port: 1883, :reconnect => false, :v311 => true, :clean_session => false,:client_id=>'ruby_client'})
+  client = MQTT::Client.connect({remote_host: MQTT_HOST,:remote_port=>MQTT_PORT, :reconnect => false, :v311 => true, :clean_session => false,:client_id=>'ruby_client'})
   return client
 end
 
 describe 'massive publish' do
+  ap 'Testing MQTT 3.1.1 spec in: %s:%s' % [MQTT_HOST,MQTT_PORT]
+
+
+
   it 'supports several connect options' do
     begin
-      client1 = MQTT::Client.connect({remote_host: HOST,remote_port: 1883, :reconnect => false, :v311 => true, :clean_session => false,:client_id=>''}){}
+      client1 = MQTT::Client.connect({remote_host: MQTT_HOST,:remote_port=>MQTT_PORT, :reconnect => false, :v311 => true, :clean_session => false,:client_id=>''}){}
     rescue Exception
     end
-    client2 = MQTT::Client.connect({remote_host: HOST,remote_port: 1883, :reconnect => false, :v311 => true, :clean_session => true,:client_id=>''}){}
+    client2 = MQTT::Client.connect({remote_host: MQTT_HOST,:remote_port=>MQTT_PORT, :reconnect => false, :v311 => true, :clean_session => true,:client_id=>''}){}
 
-    client3 = MQTT::Client.connect({remote_host: HOST,remote_port: 1883, :reconnect => false, :v311 => true, :will_topic => 'cuack1',:will_payload => 'A',:will_qos=>1,:will_retain=>true})
+    client3 = MQTT::Client.connect({remote_host: MQTT_HOST,:remote_port=>MQTT_PORT, :reconnect => false, :v311 => true, :will_topic => 'msg1',:will_payload => 'A',:will_qos=>1,:will_retain=>true})
     sleep 0.25
     client3.disconnect(false)
 
-    client4 = MQTT::Client.connect({remote_host: HOST,remote_port: 1883, :reconnect => false, :v311 => true, :will_topic => 'cuack2',:will_payload => 'A',:will_qos=>2,:will_retain=>false})
+    client4 = MQTT::Client.connect({remote_host: MQTT_HOST,:remote_port=>MQTT_PORT, :reconnect => false, :v311 => true, :will_topic => 'msg2',:will_payload => 'A',:will_qos=>2,:will_retain=>false})
     sleep 0.25
     client4.disconnect(false)
   end
@@ -37,15 +42,15 @@ describe 'massive publish' do
   it 'Subscribe and unsubscribes' do
     client = create_standard_client()
 
-    client.subscribe(['cuack0',0])
-    client.subscribe(['cuack1',1])
-    client.subscribe(['cuack2',2])
+    client.subscribe(['msg0',0])
+    client.subscribe(['msg1',1])
+    client.subscribe(['msg2',2])
 
-    client.subscribe(['cuack3',2])
-    client.unsubscribe('cuack3')
+    client.subscribe(['msg3',2])
+    client.unsubscribe('msg3')
 
-    client.subscribe(['cuack5',2],['cuack6',1],['cuack7',0])
-    client.unsubscribe('cuack5','cuack6','cuack7')
+    client.subscribe(['msg5',2],['msg6',1],['msg7',0])
+    client.unsubscribe('msg5','msg6','msg7')
 
     client.unsubscribe('inexistent_topic')
 
@@ -62,7 +67,7 @@ describe 'massive publish' do
   end
 
   it 'connects with user and password' do
-    client = MQTT::Client.connect({remote_host: HOST,remote_port: 1883, :v311 => true,:username=>'ruby_client',:password=>'password'})
+    client = MQTT::Client.connect({remote_host: MQTT_HOST,:remote_port=>MQTT_PORT, :v311 => true,:username=>'ruby_client',:password=>'password'})
 
     sleep 1
 
@@ -91,12 +96,12 @@ describe 'massive publish' do
     client_sub = create_unclean_client()
     client_pub = create_standard_client()
 
-    client_sub.subscribe(['cuack2',2],['cuack1',1],['cuack0',0],['#',1])
+    client_sub.subscribe(['msg2',2],['msg1',1],['msg0',0],['#',1])
 
-    client_pub.publish('cuack2','cuack',false,2)
-    client_pub.publish('cuack1','cuack',false,1)
-    client_pub.publish('cuack0','cuack',false,0)
-    client_pub.publish('cuack0','END',false,0)
+    client_pub.publish('msg2','msg',false,2)
+    client_pub.publish('msg1','msg',false,1)
+    client_pub.publish('msg0','msg',false,0)
+    client_pub.publish('msg0','END',false,0)
 
     loop do
       topic,message = client_sub.get()
@@ -108,10 +113,10 @@ describe 'massive publish' do
     client_sub.disconnect()
 
 
-    client_pub.publish('cuack2','cuack',true,2)
-    client_pub.publish('cuack1','cuack',true,1)
-    client_pub.publish('cuack0','cuack',true,0)
-    client_pub.publish('cuack0','END',true,0)
+    client_pub.publish('msg2','msg',true,2)
+    client_pub.publish('msg1','msg',true,1)
+    client_pub.publish('msg0','msg',true,0)
+    client_pub.publish('msg0','END',true,0)
 
     client_pub.disconnect()
 
