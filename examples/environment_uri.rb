5e8abd3801cf0f20d2d5c2872b27d840a790568d
Nicholas Humfrey <njh@aelius.com>
2014-03-23 20:05:52 +0000

    Added example of using an environment variable to specify connection settings

---
+++ examples/environment_uri.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+#
+# Connect to a MQTT broker based on a URI in
+# an environment variable
+#
+
+$:.unshift File.dirname(__FILE__)+'/../lib'
+
+require 'rubygems'
+require 'mqtt'
+
+# This environment variable may be set by the shell
+#
+# Note that you need Ruby 2.0+ to use TLS with test.mosquitto.org
+#
+ENV['MQTT_BROKER'] = 'mqtts://test.mosquitto.org:8883'
+
+MQTT::Client.connect do |client|
+  puts "Connected"
+
+  # If you pass a block to the get method, then it will loop
+  client.get('bbc/livetext/#') do |topic,message|
+    puts "#{topic}: #{message}"
+  end
+end
