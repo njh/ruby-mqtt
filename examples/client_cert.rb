c5e342e347291fe06eb85d35b81666984832d922
Nicholas Humfrey <njh@aelius.com>
2014-04-02 08:43:51 +0100

    Created example of using a client certificate to authenticate with a broker

---
+++ examples/client_cert.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+#
+# Connect to a MQTT broker using SSL/TLS Client Certificate,
+# send a single message and then receive it back
+#
+
+$:.unshift File.dirname(__FILE__)+'/../lib'
+
+require 'rubygems'
+require 'openssl'
+require 'mqtt'
+
+
+# List the supported SSL/TLS protocol versions
+p OpenSSL::SSL::SSLContext::METHODS
+
+# Ruby 1.8 / 1.9 only support TLSv1
+client = MQTT::Client.new('localhost', :ssl => :TLSv1)
+client.ca_file = './ca.pem'
+client.cert_file = './client.crt'
+client.key_file = './client.key'
+
+client.connect do
+  client.subscribe('test')
+
+  # Send a message
+  client.publish('test', "hello world")
+  
+  # If you pass a block to the get method, then it will loop
+  topic, message = client.get
+  p [topic, message]
+end
