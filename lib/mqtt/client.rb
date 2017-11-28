368ffed2492dbd73be25e252891ab15d64244f68
Jeremy Barlow <niobrara@gmx.com>
2017-11-28 10:12:43 -0800

    Use JRuby::Synchronized to synchronize socket operations under JRuby

--- lib/mqtt/client.rb
+++ lib/mqtt/client.rb
@@ -1,3 +1,5 @@
+require 'jruby/synchronized' if defined?(JRUBY_VERSION)
+
 autoload :OpenSSL, 'openssl'
 autoload :URI, 'uri'
 
@@ -165,7 +167,7 @@ module MQTT
       @read_queue = Queue.new
       @pubacks = {}
       @read_thread = nil
-      @socket_semaphore = Mutex.new
+      @write_semaphore = Mutex.new
       @pubacks_semaphore = Mutex.new
     end
 
@@ -236,6 +238,12 @@ module MQTT
           ssl_context.ssl_version = @ssl if @ssl.is_a?(Symbol)
 
           @socket = OpenSSL::SSL::SSLSocket.new(tcp_socket, ssl_context)
+
+          # Add synchronization to socket methods when running under JRuby
+          # in order to avoid read/write deadlocks.
+          # See https://github.com/jruby/jruby/issues/4854.
+          @socket.extend(JRuby::Synchronized) if defined?(JRUBY_VERSION)
+
           @socket.sync_close = true
 
           # Set hostname on secure socket for Server Name Indication (SNI)
@@ -445,12 +453,7 @@ module MQTT
       result = IO.select([@socket], [], [], SELECT_TIMEOUT)
       unless result.nil?
         # Yes - read in the packet
-        packet = nil
-        # Only allow one thread to do writes and/or blocking reads to the
-        # socket at a time. Threads could otherwise deadlock.
-        @socket_semaphore.synchronize do
-          packet = MQTT::Packet.read(@socket)
-        end
+        packet = MQTT::Packet.read(@socket)
         handle_packet packet
       end
       keep_alive!
@@ -518,10 +521,8 @@ module MQTT
       # Raise exception if we aren't connected
       raise MQTT::NotConnectedException unless connected?
 
-      # Only allow one thread to do writes and/or blocking reads to the
-      # socket at a time. Threads could otherwise deadlock or, in the case
-      # of concurrent writes, interleave data inappropriately.
-      @socket_semaphore.synchronize do
+      # Only allow one thread to write to socket at a time
+      @write_semaphore.synchronize do
         @socket.write(data.to_s)
       end
     end
